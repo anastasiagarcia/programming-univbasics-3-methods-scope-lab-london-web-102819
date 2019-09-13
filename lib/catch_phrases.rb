6ffeeaf78685be7af9d0d0cac767e32c3176feea
Anastasia Pankina <anastaciap@ymail.com>
2019-09-13 18:12:06 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,10 @@
 $status = "Thank You Mario! But Our Princess Is In Another Castle!"
 
-def mario
+def mario()
   phrase="It's-a me, Mario!"
   puts phrase
 end
 
-def toadstool
+def toadstool()
   puts $status
 end
